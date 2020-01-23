2c5d4a046c104bd5f6eec30e6ebbfbd6bccb2b60
Amar1729 <amar.paul16@gmail.com>
2020-01-23 14:41:36 -0500

    fix spotify script when not playing a song

--- scripts/playing.sh
+++ scripts/playing.sh
@@ -5,7 +5,13 @@ spotify="/usr/local/bin/spotify"
 main () {
     if command -v $spotify &>/dev/null; then
         if [[ $(osascript -e 'application "Spotify" is running') == "true" ]]; then
-            echo "$($spotify status track) - $($spotify status artist)"
+            track="$($spotify status track 2>/dev/null)"
+            artist="$($spotify status artist 2>/dev/null)"
+            if [[ -z "$track" ]]; then
+                echo "spotify = paused"
+            else
+                echo "$track - $artist"
+            fi
         else
             echo "spotify = ded"
         fi
